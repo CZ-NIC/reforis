ca53bd5deb54dfa323b1d4b6870b1ec81b979eae
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-03-23 15:54:19 +0100

    Better react apps management.

---
+++ js/src/app.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+import Wifi from './wifi/Wifi';
+import NotificationsDropdown from './notifications/NotificationsDropdown';
+
+window.addEventListener('load', () => {
+    const apps = [
+        {id: 'notifications_dropdown_container', component: NotificationsDropdown},
+        {id: 'wifi_form_container', component: Wifi},
+    ];
+
+    for (let app of apps) {
+        const appElm = document.getElementById(app.id);
+        if (!appElm) continue;
+        render(<app.component/>, appElm);
+    }
+}, false);
