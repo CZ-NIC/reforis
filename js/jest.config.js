cf4372976c27b1a82cf7aad5fb37694513c2dc95
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-04-11 14:46:03 +0200

    Add jest config.

---
+++ js/jest.config.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+// https://jestjs.io/docs/en/configuration.html
+module.exports = {
+    clearMocks: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    setupFiles: ['<rootDir>/src/testUtils/setupGlobals.js'],
+    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+    verbose: false,
+};
