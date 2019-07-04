c88c7379a75fee569d409bfae385287954316674
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-07-04 18:40:38 +0200

    Update to v0.3.2.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name='reforis',
-    version='0.3.1',
+    version='0.3.2',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
