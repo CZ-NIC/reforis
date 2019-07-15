3b23397ffd96ccd473f9e14eb316811227315f95
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-07-15 14:09:07 +0200

    Bump v0.3.6.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name='reforis',
-    version='0.3.5',
+    version='0.3.6',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
