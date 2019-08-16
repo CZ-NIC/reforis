6e6f88193714a58f18b22aa4c87f1291b425acea
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-08-16 17:33:41 +0200

    Add plugins routing and navigation utils.

---
+++ js/src/utils/__tests__/__fixtures__/plugins.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+import React from 'react';
+
+export const pluginInRoot = weight => ({
+    weight: weight,
+    name: 'Plugin in root',
+    path: '/plugin-in-root',
+    component: () => <p>Root</p>,
+});
+
+export const pluginInExistedSubmenu = (submenuId, weight) => ({
+    path: '/plugin-in-submenu',
+    name: 'Plugin in submenu',
+    submenuId: submenuId,
+    weight: weight,
+    component: () => <p>SubPlugin</p>,
+});
+
+export const newSubmenu = (submenuId, weight) => ({
+    submenuId: submenuId,
+    name: 'New Submenu',
+    weight: weight,
+    path: '/plugin-in-submenu',
+    pages: [
+        {
+            name: 'SubPlugin one',
+            path: '/sub-plugin-one',
+            component: () => <p>SubPlugin One</p>
+        }, {
+
+            name: 'SubPlugin two',
+            path: '/sub-plugin-two',
+            component: () => <p>SubPlugin Two</p>
+        },
+    ]
+});
---
+++ js/src/utils/pages.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+export function addWeightsToPages(pages) {
+    const pagesLength = pages.length;
+    const step = Math.floor(100 / pagesLength);
+    let i = 0;
+    return pages.map((page) => {
+        if (Object.prototype.hasOwnProperty.call(page, 'pages'))
+            page.pages = addWeightsToPages(page.pages);
+        i++;
+        return {...page, weight: i * step}
+    });
+}
+
+export function plug(pages, plugin) {
+    let copiedPages = [...pages];
+    let menuToInsert;
+    if (Object.hasOwnProperty.call(plugin, 'submenuId')) {
+        if (Object.hasOwnProperty.call(plugin, 'pages')) {
+            plugin.pages = addWeightsToPages(plugin.pages);
+            menuToInsert = copiedPages;
+        } else
+            menuToInsert = copiedPages.find(item => item.submenuId === plugin.submenuId).pages;
+    } else
+        menuToInsert = copiedPages;
+    menuToInsert.push(plugin);
+    menuToInsert.sort((a, b) => a.weight - b.weight);
+    return copiedPages;
+}
