b376d46305a103ebc2f65f5faa27086b0cd41e89
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-09-27 15:35:42 +0200

    Fix styleguide.

--- js/styleguide.config.js
+++ js/styleguide.config.js
@@ -5,110 +5,91 @@
  * See /LICENSE for more information.
  */
 
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    title: 'reForis JS docs',
+    title: "Foris JS docs",
     sections: [
         {
-            name: 'Introduction',
-            content: 'docs/introduction.md'
+            name: "Introduction",
+            content: "docs/introduction.md",
         },
         {
-            name: 'Foris forms system',
+            name: "Foris forms components",
             components: [
-                'src/formContainer/ForisForm.js',
-                'src/formContainer/alerts.js',
-                'src/formContainer/SubmitButton.js',
-
+                "src/lan/LAN.js",
+                "src/wan/WAN.js",
+                "src/wifi/WiFi.js",
+                "src/dns/DNS.js",
+                "src/guestNetwork/GuestNetwork.js",
+                "src/interfaces/Interfaces.js",
+                "src/notificationSettings/NotificationSettings.js",
+                "src/packageManagement/packages/Packages.js",
+                "src/regionAndTime/RegionAndTime.js",
+                "src/packageManagement/updateSettings/UpdateSettings.js",
+                "src/packageManagement/updates/Updates.js",
+                "src/password/Password.js",
             ],
-            exampleMode: 'expand',
-            usageMode: 'expand',
+            exampleMode: "expand",
+            usageMode: "expand",
         },
         {
-            name: 'Foris forms components',
+            name: "Top bar components",
             components: [
-                'src/lan/LAN.js',
-                'src/wan/WAN.js',
-                'src/wifi/WiFi.js',
-                'src/dns/DNS.js',
-                'src/guestNetwork/GuestNetwork.js',
-                'src/interfaces/Interfaces.js',
-                'src/notificationSettings/NotificationSettings.js',
-                'src/packageManagement/packages/Packages.js',
-                'src/regionAndTime/RegionAndTime.js',
-                'src/packageManagement/updateSettings/UpdateSettings.js',
-                'src/packageManagement/updates/Updates.js',
-                'src/password/Password.js',
+                "src/languagesDropdown/LanguagesDropdown.js",
+                "src/notifications/NotificationsDropdown/NotificationsDropdown.js",
             ],
-            exampleMode: 'expand',
-            usageMode: 'expand',
+            exampleMode: "expand",
+            usageMode: "expand",
         },
         {
-            name: 'Top bar components',
+            name: "Router state handlers",
             components: [
-                'src/languagesDropdown/LanguagesDropdown.js',
-                'src/notifications/NotificationsDropdown/NotificationsDropdown.js',
+                "src/routerStateHandler/RouterStateHandler.js",
+                "src/routerStateHandler/NetworkRestartHandler.js",
+                "src/routerStateHandler/RebootHandler.js",
             ],
-            exampleMode: 'expand',
-            usageMode: 'expand',
+            exampleMode: "expand",
+            usageMode: "expand",
         },
         {
-            name: 'Router state handlers',
+            name: "Other Foris components",
             components: [
-                'src/routerStateHandler/RouterStateHandler.js',
-                'src/routerStateHandler/NetworkRestartHandler.js',
-                'src/routerStateHandler/RebootHandler.js',
+                "src/overview/Overview.js",
+                "src/overview/UpdatesApprovals.js",
+                "src/notifications/Notifications/Notifications.js",
+                "src/connectionTest/ConnectionTest.js",
+                "src/reboot/Reboot.js",
+                "src/guide/Guide.js",
             ],
-            exampleMode: 'expand',
-            usageMode: 'expand',
+            exampleMode: "expand",
+            usageMode: "expand",
         },
-        {
-            name: 'Other Foris components',
-            components: [
-                'src/overview/Overview.js',
-                'src/overview/UpdatesApprovals.js',
-                'src/notifications/Notifications/Notifications.js',
-                'src/connectionTest/ConnectionTest.js',
-                'src/reboot/Reboot.js',
-                'src/guide/Guide.js',
-            ],
-            exampleMode: 'expand',
-            usageMode: 'expand',
-        },
-        {
-            name: 'Bootstrap components',
-            description: 'Set of bootstrap components.',
-            components: 'src/common/bootstrap/*.js',
-            exampleMode: 'expand',
-            usageMode: 'expand',
-            ignore: [
-                'src/common/bootstrap/constants.js'
-            ]
-        }
     ],
     require: [
-        'babel-polyfill',
-        path.join(__dirname, '../reforis_static/reforis/css/bootstrap.min.css'),
-        path.join(__dirname, '../reforis_static/reforis/css/react-datatime.css'),
-        path.join(__dirname, '../reforis_static/reforis/css/fontawesome.min.css'),
-        './src/testUtils/setupGlobals.js',
+        "./src/testUtils/setupGlobals.js",
     ],
     webpackConfig: {
+        resolve: {
+            modules: [
+                path.resolve(__dirname, "./src"),
+                path.resolve(__dirname, "./node_modules"),
+            ],
+        },
         module: {
             rules: [
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
-                    loader: 'babel-loader'
+                    loader: "babel-loader",
                 }, {
                     test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
+                    use: ["style-loader", "css-loader"],
                 }, {
                     test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-                    loader: 'file-loader'
+                    loader: "file-loader",
                 },
-            ]
+            ],
         },
-    }
+    },
 };
