cbd88eced441c728b07415c08bc75e35f88d256f
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-10-01 11:51:49 +0200

    Mock css in jest.

---
+++ js/src/__mocks__/styleMock.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+module.exports = {};
