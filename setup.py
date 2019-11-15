daadcecabbcb4ffa847c5c1681bd9ee4cdeabcb8
Maciej Lenartowicz <maciej.lenartowicz@nic.cz>
2019-11-15 13:44:20 +0100

    Version bump - 0.5.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name='reforis',
-    version='0.4.5',
+    version='0.5.0',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
