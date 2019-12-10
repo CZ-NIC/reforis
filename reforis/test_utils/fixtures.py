061e204d38682dc41e0ab3a7a65beebed59648ce
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-12-10 13:27:57 +0100

    Move fixtures to test_utils modules.
    It allows to use client and other fixtures in plugin tests.

--- reforis/test_utils/fixtures.py
+++ reforis/test_utils/fixtures.py
@@ -1,7 +1,10 @@
 import pytest
 from flask import Flask, jsonify
 from flask_babel import Babel
+from unittest import mock
 
+from reforis import create_app
+from reforis.test_utils import send_mock, surrogate
 from reforis.foris_controller_api.utils import APIError
 
 
@@ -19,3 +22,46 @@ def app_with_blueprint():
         return app
 
     return app_factory
+
+
+@pytest.fixture
+def client():
+    """
+    Flask's client to make requests. For example:
+
+    .. code-block:: python
+
+        def test_foo(client):
+            res = client.get('/api/foo')
+            assert res.status_code == 200
+            assert res.json == {...}
+    """
+    app = _stubbed_app()
+    with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['logged'] = True
+        yield client
+
+
+@pytest.fixture
+def request_ctx():
+    """
+    Use this fixture when you call functions directly but they need request
+    context like access to logged user, language and so on.
+    """
+    app = _stubbed_app()
+
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        yield
+
+
+@surrogate('foris_client.buses.mqtt.MqttSender')
+@surrogate('foris_client.buses.base.ControllerError')
+@mock.patch('foris_client.buses.mqtt.MqttSender')
+@mock.patch('reforis.backend.MQTTBackend._parse_credentials', mock.Mock)
+def _stubbed_app(sender_mock=None):
+    sender_mock.return_value.send.side_effect = send_mock
+    # The foris client may not existed on the testing environment
+    # so we surrogate this module to avoid ImportError during testing.
+    return create_app('test')
