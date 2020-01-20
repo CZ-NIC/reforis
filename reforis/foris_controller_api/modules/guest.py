bb7a3981bd1c7970754f33ae0dd944f8089a238c
Maciej Lenartowicz <maciej.lenartowicz@nic.cz>
2020-01-20 17:36:11 +0100

    Fixed lease time bug for guest network.

--- reforis/foris_controller_api/modules/guest.py
+++ reforis/foris_controller_api/modules/guest.py
@@ -1,4 +1,4 @@
-from reforis.foris_controller_api.utils import _foris_controller_settings_call
+from flask import current_app, jsonify, request
 
 
 def guest_network():
@@ -13,7 +13,19 @@ def guest_network():
         See ``update_settings`` action in the `foris-controller guest module JSON schema
         <https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller/blob/master/foris_controller_modules/guest/schema/guest.json>`_.
     """
-    return _foris_controller_settings_call('guest')
+    response = None
+    if request.method == 'GET':
+        response = current_app.backend.perform('guest', 'get_settings')
+        if response['dhcp']['enabled'] is True:
+            # Convert seconds to hours
+            response['dhcp']['lease_time'] /= 3600
+    elif request.method == 'POST':
+        data = request.json
+        if data['dhcp']['enabled'] is True:
+            # Convert hours to seconds
+            data['dhcp']['lease_time'] *= 3600
+        response = current_app.backend.perform('guest', 'update_settings', data)
+    return jsonify(response)
 
 
 # pylint: disable=invalid-name
