097557e7b26429d41fbcdeb27ba73b7679d3c10f
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-05-13 15:39:29 +0200

    Split guest-network endpoint into GET and POST ones.

--- reforis/foris_controller_api/modules/guest.py
+++ reforis/foris_controller_api/modules/guest.py
@@ -1,37 +1,43 @@
 from flask import current_app, jsonify, request
 
 
-def guest_network():
+def guest_network_get():
     """
     .. http:get:: /api/guest-network
         Get `guest network` router settings.
         See ``get_settings`` action in the `foris-controller guest module JSON schema
         <https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller/blob/master/foris_controller_modules/guest/schema/guest.json>`_.
+    """
+    response = current_app.backend.perform('guest', 'get_settings')
+    if response['dhcp']['enabled'] is True:
+        # Convert seconds to hours
+        response['dhcp']['lease_time'] /= 3600
+    return jsonify(response)
 
+
+def guest_network_set():
+    """
     .. http:post:: /api/guest-network
         Get `guest network` router settings.
         See ``update_settings`` action in the `foris-controller guest module JSON schema
         <https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller/blob/master/foris_controller_modules/guest/schema/guest.json>`_.
     """
-    response = None
-    if request.method == 'GET':
-        response = current_app.backend.perform('guest', 'get_settings')
-        if response['dhcp']['enabled'] is True:
-            # Convert seconds to hours
-            response['dhcp']['lease_time'] /= 3600
-    elif request.method == 'POST':
-        data = request.json
-        # if enabled=False other 'dhcp' part is missing
-        if data['enabled'] and data['dhcp']['enabled'] is True:
-            # Convert hours to seconds
-            data['dhcp']['lease_time'] *= 3600
-        response = current_app.backend.perform('guest', 'update_settings', data)
+    data = request.json
+    # if enabled=False other 'dhcp' part is missing
+    if data['enabled'] and data['dhcp']['enabled'] is True:
+        # Convert hours to seconds
+        data['dhcp']['lease_time'] *= 3600
+    response = current_app.backend.perform('guest', 'update_settings', data)
     return jsonify(response)
 
 
 # pylint: disable=invalid-name
 views = [{
     'rule': '/guest-network',
-    'view_func': guest_network,
-    'methods': ['GET', 'POST']
+    'view_func': guest_network_get,
+    'methods': ['GET']
+}, {
+    'rule': '/guest-network',
+    'view_func': guest_network_set,
+    'methods': ['POST']
 }]
