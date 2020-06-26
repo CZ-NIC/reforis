8dcda38b249a0b97ac29b13bd15c2b4136ab5f4a
Marek Sašek <marek.sasek@nic.cz>
2020-06-26 17:16:18 +0200

    Add icon to Overview menu item

---
+++ js/src/overview/Overview.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See /LICENSE for more information.
+ */
+
+export default function Overview() {
+    return <p>hello ooo</p>
+}
