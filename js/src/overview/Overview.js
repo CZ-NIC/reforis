fb44d3adaf14870ba948d41c5625653356bf7edb
Marek Sašek <marek.sasek@nic.cz>
2020-07-03 16:37:41 +0200

    Create initial sketch of Connection Test card

--- js/src/overview/Overview.js
+++ js/src/overview/Overview.js
@@ -8,5 +8,34 @@
 import React from "react";
 
 export default function Overview() {
-    return <h1>Overview</h1>;
+    const icon = <i className="fas fa-check-circle float-right mr-2 mt-1" />;
+    return (
+        <>
+            <h1>Overview</h1>
+            <div className="card border-secondary mb-3" style={{ maxWidth: "18rem" }}>
+                <h5 className="card-header">Connection Test</h5>
+                <div className="card-body">
+                    <p className="card-text">
+                        IPv4 connectivity
+                        {icon}
+                    </p>
+                    <p className="card-text">
+                        IPv4 gateway connectivity
+                        {icon}
+                    </p>
+                    <p className="card-text">
+                        IPv6 connectivity
+                        {icon}
+                    </p>
+                    <p className="card-text">
+                        IPv6 gateway connectivity
+                        {icon}
+                    </p>
+                    <div className="text-center">
+                        <a href="javascript;" className="btn btn-primary mt-2">Test connection</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
