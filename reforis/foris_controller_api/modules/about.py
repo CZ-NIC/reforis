2070781417de6dda0caf0ece4e4b22414e219d1a
Martin Matějek <martin.matejek@nic.cz>
2020-07-22 17:09:59 +0200

    Add reforis version info to about endpoint

--- reforis/foris_controller_api/modules/about.py
+++ reforis/foris_controller_api/modules/about.py
@@ -1,4 +1,5 @@
 import os
+import pkg_resources
 
 from flask import current_app, jsonify
 
@@ -11,6 +12,8 @@ def about():
     """
     data = current_app.backend.perform('about', 'get')
     data['serial'] = int(data['serial'], 16)
+    # additional reforis version info
+    data['reforis_version'] = pkg_resources.get_distribution('reforis').version
     return jsonify(data)
 
 
