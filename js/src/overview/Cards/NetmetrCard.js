bfef27b3c6a0726217828eac3ef097c4601693b8
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-07-22 10:38:06 +0200

    Add noopener noreferrer for netmetr details

--- js/src/overview/Cards/NetmetrCard.js
+++ js/src/overview/Cards/NetmetrCard.js
@@ -35,76 +35,74 @@ function NetmetrCard({ tests }) {
         .unix(lastTest.time)
         .locale(ForisTranslations.locale)
         .format("HH:mm DD.MM.YYYY");
+
     return (
-        <>
-            <div className="col mb-4">
-                <div className="card h-100">
-                    <div className="card-body">
-                        <h6 className="text-uppercase text-muted mb-2">
-                            NetMetr
-                            <a href="javasript;" className="text-secondary">
-                                <i className="fas fa-ellipsis-v float-right" />
-                            </a>
-                        </h6>
-                        <form>
-                            <table className="table table-borderless table-hover offset-lg-3 col-lg-6 col-sm-12">
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">
-                                            <span>Download [Mb/s]</span>
-                                        </th>
-                                        <td style={{ textAlign: "right" }}>
-                                            <span>
-                                                {lastTest.speed_download}
-                                            </span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <span>Upload [Mb/s]</span>
-                                        </th>
-                                        <td style={{ textAlign: "right" }}>
-                                            <span>{lastTest.speed_upload}</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <span>Ping [ms]</span>
-                                        </th>
-                                        <td style={{ textAlign: "right" }}>
-                                            <span>{lastTest.ping}</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <span>Date</span>
-                                        </th>
-                                        <td style={{ textAlign: "right" }}>
-                                            <span>{time}</span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">
-                                            <span>Link</span>
-                                        </th>
-                                        <td style={{ textAlign: "right" }}>
-                                            <a
-                                                target="_blank"
-                                                href={`https://www.netmetr.cz/en/detail.html?${lastTest.test_uuid}`}
-                                            >
-                                                {_("Details")}
-                                                &nbsp;
-                                                <i className="fas fa-external-link-alt" />
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </form>
-                    </div>
+        <div className="col mb-4">
+            <div className="card h-100">
+                <div className="card-body">
+                    <h6 className="text-uppercase text-muted mb-2">
+                        NetMetr
+                        <a href="javasript;" className="text-secondary">
+                            <i className="fas fa-ellipsis-v float-right" />
+                        </a>
+                    </h6>
+                    <form>
+                        <table className="table table-borderless table-hover offset-lg-3 col-lg-6 col-sm-12">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <span>Download [Mb/s]</span>
+                                    </th>
+                                    <td style={{ textAlign: "right" }}>
+                                        <span>{lastTest.speed_download}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <span>Upload [Mb/s]</span>
+                                    </th>
+                                    <td style={{ textAlign: "right" }}>
+                                        <span>{lastTest.speed_upload}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <span>Ping [ms]</span>
+                                    </th>
+                                    <td style={{ textAlign: "right" }}>
+                                        <span>{lastTest.ping}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <span>Date</span>
+                                    </th>
+                                    <td style={{ textAlign: "right" }}>
+                                        <span>{time}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <span>Link</span>
+                                    </th>
+                                    <td style={{ textAlign: "right" }}>
+                                        <a
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            href={`https://www.netmetr.cz/en/detail.html?${lastTest.test_uuid}`}
+                                        >
+                                            {_("Details")}
+                                            &nbsp;
+                                            <i className="fas fa-external-link-alt" />
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </form>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
