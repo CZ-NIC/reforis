78bc207d51d343fd7acfcb7829b3a37111057aaf
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-07-24 15:27:12 +0200

    Add responsive breakpoints to Overview page

--- js/src/overview/Overview.js
+++ js/src/overview/Overview.js
@@ -56,7 +56,7 @@ function OverviewCards({ packages, ws }) {
     return (
         <>
             <h1>Overview</h1>
-            <div className="row row-cols-1 row-cols-md-3 mt-4">
+            <div className="row row-cols-1 row-cols-lg-3 mt-4">
                 <AutomaticUpdatesCard />
                 <DataCollectionCard />
                 {displayCard(packages, "openvpn") ? <OpenVPNCard /> : null}
