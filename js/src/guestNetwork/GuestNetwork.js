169cf7eca2e4cab2e51c53b3b63a844aace13070
Marek Sašek <marek.sasek@nic.cz>
2020-08-07 15:32:41 +0200

    Add guest network warning

--- js/src/guestNetwork/GuestNetwork.js
+++ js/src/guestNetwork/GuestNetwork.js
@@ -9,15 +9,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import {
-    ForisForm, validateIPv4Address, ForisURLs, undefinedIfEmpty, withoutUndefinedKeys,
+    ForisForm,
+    validateIPv4Address,
+    ForisURLs,
+    undefinedIfEmpty,
+    withoutUndefinedKeys,
 } from "foris";
 
 import API_URLs from "common/API";
-import { validateNetworkMask, validateRequiredField } from "common/network/validators";
+import {
+    validateNetworkMask,
+    validateRequiredField,
+} from "common/network/validators";
 import validateDHCP from "common/network/DHCPValidators";
 import { getDHCPStart } from "common/network/utils";
 import GuestNetworkForm, { validateQoS } from "./GuestNetworkForm";
 import GuestNetworkDHCPClientsList from "./GuestNetworkDHCPClientsList";
+import GuestNetworkNotification from "./GuestNetworkNotification";
 
 GuestNetwork.propTypes = {
     ws: PropTypes.object.isRequired,
@@ -27,14 +35,16 @@ export default function GuestNetwork({ ws }) {
     return (
         <>
             <h1>{_("Guest Network")}</h1>
-            <p dangerouslySetInnerHTML={{
-                __html: _(`
+            <div id="guest-notification" />
+            <p
+                dangerouslySetInnerHTML={{
+                    __html: _(`
 Guest network is used for <a href="${ForisURLs.wifi}">guest Wi-Fi</a>.
 It is separated from your ordinary LAN. Devices connected to this network are allowed to access the
 internet, but are not allowed to access the configuration interface of the this device nor the devices
 in LAN.
         `),
-            }}
+                }}
             />
             <ForisForm
                 ws={ws}
@@ -48,6 +58,7 @@ in LAN.
             >
                 <GuestNetworkForm />
                 <GuestNetworkDHCPClientsList />
+                <GuestNetworkNotification />
             </ForisForm>
             <div id="dhcp-clients-container" />
         </>
@@ -55,11 +66,13 @@ in LAN.
 }
 
 function prepData(formData) {
-    delete formData.interface_count;
-    delete formData.interface_up_count;
     // eslint-disable-next-line no-restricted-globals
-    if (!isNaN(formData.dhcp.start)) { // Be sure to convert start address only once.
-        formData.dhcp.start = getDHCPStart(formData.ip, formData.dhcp.start).toString();
+    if (!isNaN(formData.dhcp.start)) {
+        // Be sure to convert start address only once.
+        formData.dhcp.start = getDHCPStart(
+            formData.ip,
+            formData.dhcp.start,
+        ).toString();
     }
     return formData;
 }
@@ -80,15 +93,13 @@ export function prepDataToSubmit(formData) {
 
 export function validator(formData) {
     const errors = {
-        ip: (
+        ip:
             validateRequiredField(formData.ip)
-            || validateIPv4Address(formData.ip)
-        ),
-        netmask: (
+            || validateIPv4Address(formData.ip),
+        netmask:
             validateRequiredField(formData.netmask)
             || validateIPv4Address(formData.netmask)
-            || validateNetworkMask(formData.netmask)
-        ),
+            || validateNetworkMask(formData.netmask),
     };
 
     if (formData.qos.enabled) {
@@ -97,7 +108,10 @@ export function validator(formData) {
 
     if (formData.dhcp.enabled) {
         errors.dhcp = validateDHCP(
-            formData.dhcp, formData.ip, formData.netmask, errors.ip || errors.netmask,
+            formData.dhcp,
+            formData.ip,
+            formData.netmask,
+            errors.ip || errors.netmask,
         );
     }
 
