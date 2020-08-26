df6bd9c96c29b3d3e3d2a16d119e00792d17a3d9
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-08-26 15:12:07 +0200

    fixup! Fix align of notifications dropdown's header

--- js/src/main/TopBar/rebootDropdown/RebootDropdown.js
+++ js/src/main/TopBar/rebootDropdown/RebootDropdown.js
@@ -1,12 +1,15 @@
 /*
- * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
  */
 
 import React from "react";
+
 import PropTypes from "prop-types";
+import { ForisURLs } from "foris";
+import { Link } from "react-router-dom";
 
 import useNotifications from "notifications/hooks";
 import RebootButton from "common/RebootButton";
