b000caaf47823635cdbcd294606451df4a6d6d5d
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-09-08 15:16:57 +0200

    Fix not active navigation toggle collapse

--- js/src/navigation/NavigationToggle.js
+++ js/src/navigation/NavigationToggle.js
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useUID } from "react-uid";
 
@@ -25,6 +25,7 @@ NavigationToggle.propTypes = {
 export function NavigationToggle({ name, icon, active, children }) {
     const uid = useUID();
     const iconElement = getIconElement(icon);
+    const [activeToggle, setActiveToggle] = useState(active);
 
     return (
         <li>
@@ -32,12 +33,15 @@ export function NavigationToggle({ name, icon, active, children }) {
                 className="dropdown-toggle"
                 href={`#nav-toggle-${uid}`}
                 data-toggle="collapse"
+                onLoad={() => setActiveToggle(false)}
             >
                 {iconElement}
                 {name}
             </a>
             <ul
-                className={`collapse list-unstyled ${active ? "show" : ""}`}
+                className={`collapse list-unstyled ${
+                    activeToggle && "show"
+                }`.trim()}
                 id={`nav-toggle-${uid}`}
             >
                 {children}
