e0bb60a48880d92b05afe218e25f67e1c2eb7dbb
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-09-08 17:05:12 +0200

    Fix tests

--- js/jest.config.js
+++ js/jest.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -7,12 +7,10 @@
 
 // https://jestjs.io/docs/en/configuration.html
 module.exports = {
-    moduleDirectories: [
-        "node_modules",
-        "<rootDir>/src/",
-    ],
+    moduleDirectories: ["node_modules", "<rootDir>/src/"],
     moduleNameMapper: {
         "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js",
+        "^lodash-es$": "lodash",
     },
     clearMocks: true,
     collectCoverageFrom: ["src/**/*.{js,jsx}"],
