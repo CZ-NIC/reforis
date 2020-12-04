0fe409d1ac79bc43359d35751c43a573dbf9a64b
Filip Hron <filip.hron@nic.cz>
2020-12-04 16:45:16 +0000

    Fix backward compatibility with plugins.
    Co-authored-by: Bogdan Bodnar <bogdan.bodnar@nic.cz>

---
+++ reforis/foris_controller_api/utils.py
@@ -0,0 +1,10 @@
+#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#
+#  This is free software, licensed under the GNU General Public License v3.
+#  See /LICENSE for more information.
+
+# pylint: disable=unused-import
+
+# Fix backward compatibility with plugins
+from reforis.foris_controller_api.modules.utils import validate_json
+from reforis.utils import log_error, APIError
---
+++ reforis/utils.py
@@ -0,0 +1,26 @@
+#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#
+#  This is free software, licensed under the GNU General Public License v3.
+#  See /LICENSE for more information.
+
+from http import HTTPStatus
+
+from flask import request, current_app
+
+
+class APIError(Exception):
+    """
+    Raised when an error occurred during processing request.
+    """
+
+    def __init__(self, data, status_code=HTTPStatus.BAD_REQUEST):
+        super().__init__(self)
+        self.data = data
+        self.status_code = status_code
+
+
+def log_error(message):
+    """
+    Report error using logger from current application. Request URL and data are added to the message.
+    """
+    current_app.logger.error('%s; URL: %s; data: %s', message, request.url, request.data)
