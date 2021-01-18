1705f64776d834eae631acd9f64bd6ccfe435363
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-01-18 23:46:31 +0100

    Bump v1.0.4
    * Add syslog settings
    * Fix IPv6 prefix as not required
    * Fix headings structure on multiple pages
    * Fix formFieldsSize structure of multiple cards
    * Fix several typos and messages in texts
    * Update Foris JS to version 5.1.11
    * Update & add new translations
    * Remove spinner on Main page
    * Remove duplicated Norwegian language
    * Remove SimpleBar's autoHide parameter

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name='reforis',
-    version='1.0.3',
+    version='1.0.4',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
