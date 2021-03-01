50f49153bc92b0ad9bb1b1ffce66e970672b8b51
Filip Hron <filip.hron@nic.cz>
2021-03-01 14:52:02 +0000

    errors: return JSON instead of HTML from flask

--- reforis/__init__.py
+++ reforis/__init__.py
@@ -108,12 +108,18 @@ def create_app(config):
     return app
 
 
+def _wrap_error(error, code):
+    if isinstance(error, dict):
+        return dict({'status': code}, **error)
+    return {'status': code, 'error': 'Error:{!r}'.format(error)}
+
+
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+    return _wrap_error(error, 404)
 
 
 def internal_error(error):
-    return render_template('errors/500.html', error=error), 500
+    return _wrap_error(error, 500)
 
 
 def foris_controller_error(e):
@@ -132,9 +138,9 @@ def foris_controller_error(e):
     if e.remote_description.startswith('Incorrect input.'):
         # indicates incorrect input, not actually a server error
         # but a client error so we'll return more appropriate status code
-        return render_template('errors/400.html', **error), 400
+        return _wrap_error(error, 400)
 
-    return render_template('errors/500.html', **error), 500
+    return _wrap_error(error, 500)
 
 
 def set_backend(app):
