45221b37e3b2028ea21de3b68c6c51bce1b5db6e
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-05-31 15:37:03 +0200

    Add external link to UserOptions

--- js/src/packageManagement/packages/UserOptions/UserOptions.js
+++ js/src/packageManagement/packages/UserOptions/UserOptions.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2020-2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -30,6 +30,7 @@ export default function UserOptions({
                     {...option}
                     index={index}
                     packageIndex={packageIndex}
+                    url={option.url}
                     setFormValue={setFormValue}
                     disabled={disabled}
                     key={option.name}
